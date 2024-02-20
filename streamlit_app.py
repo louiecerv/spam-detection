434d05c1e124afcdb32966ff4680ec0e2cf0c404
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-02-21 05:20:39 +0800

    Save

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,12 +25,13 @@ def app():
     y = data['v1']        
     
     clfNB = make_pipeline(TfidfVectorizer(), MultinomialNB())
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
+    clfNB.fit(X_train, y_train)
     
+    X_test_pred = clfNB.predict(X_test)
     if st.button('Start'):
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
-        clfNB.fit(X_train, y_train)
-        X_test_pred = clfNB.predict(X_test)
+
 
         accuracy = 100.0 * (y_test == X_test_pred).sum() / X_test.shape[0]
         st.write("Accuracy of the NB classifier =", round(accuracy, 2), "%")
